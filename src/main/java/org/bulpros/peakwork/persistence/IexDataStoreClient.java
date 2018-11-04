b8fd3850dae6b1dcfb6e2908077f61b7a946a415
Stefan Tabakov <stefan.tabakov@bulpros.com>
2018-11-04 18:48:39 +0200

    Added retrieveStock implementation using appengine. Added .gitignore. Removed files in target folder.

--- src/main/java/org/bulpros/peakwork/persistence/IexDataStoreClient.java
+++ src/main/java/org/bulpros/peakwork/persistence/IexDataStoreClient.java
@@ -2,82 +2,80 @@ package org.bulpros.peakwork.persistence;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.SortDirection;
+
 import com.google.cloud.Timestamp;
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.Key;
-import com.google.cloud.datastore.KeyFactory;
-import com.google.cloud.datastore.Query;
 import com.google.cloud.datastore.StringValue;
-import com.google.cloud.datastore.StructuredQuery.CompositeFilter;
-import com.google.cloud.datastore.StructuredQuery.OrderBy;
-import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
 
 import pl.zankowski.iextrading4j.api.stocks.BatchStocks;
 
 @Component
-public class 
-IexDataStoreClient {
+public class IexDataStoreClient {
 
-	// Create an authorized Datastore service using Application Default Credentials.
-	private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    // Create an authorized Datastore service using Application Default Credentials.
+    private final DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+    
 
-	// Create a Key factory to construct keys associated with this project.
-	private final KeyFactory keyFactory = datastore.newKeyFactory().setKind("stock");
+    public Key addStock(String symbol,BatchStocks stockData) {
+        Entity stock = new Entity("stock");
+                        stock.setProperty("symbol", symbol);
+                        stock.setProperty("companyName", stockData.getKeyStats().getCompanyName());
+                        stock.setProperty("price", stockData.getPrice().toEngineeringString());
+                        stock.setProperty("logo", StringValue.newBuilder(stockData.getLogo().getUrl()).setExcludeFromIndexes(true).build());
+                        stock.setProperty("created", Timestamp.now());
+                        
+        return datastoreService.put(stock);
+    }
 
-	public Key addStock(String symbol,BatchStocks stockData) {
-		Key key = datastore.allocateId(keyFactory.newKey());
-		Entity stock = Entity.newBuilder(key)
-				.set("symbol", symbol)
-				.set("companyName", stockData.getKeyStats().getCompanyName())
-				.set("price", stockData.getPrice().toEngineeringString())
-				.set("logo", StringValue.newBuilder(stockData.getLogo().getUrl()).setExcludeFromIndexes(true).build())
-				.set("created", Timestamp.now())
-				.build();
-		datastore.put(stock);
-		return key;
-	}
+    public List<String> retrieveStock(String[] symbols, Date fromDate, Date toDate) {
+        Query query = new Query("stock");
+        List<Filter> symbolFilters = new ArrayList<Filter>();
+        for (int i = 1; i < symbols.length; i++) {
+            symbolFilters.add(new Query.FilterPredicate("symbol", FilterOperator.EQUAL, symbols[i]));
+        }
+        Query.CompositeFilter symbolFilter = new Query.CompositeFilter(Query.CompositeFilterOperator.OR, symbolFilters);
+        
+        List<Filter> dateFilters = new ArrayList<Filter>();
+        dateFilters.add(new Query.FilterPredicate("created", FilterOperator.GREATER_THAN_OR_EQUAL, fromDate));
+        dateFilters.add(new Query.FilterPredicate("created", FilterOperator.LESS_THAN_OR_EQUAL, toDate));
+        
+        Query.CompositeFilter dateFilter = new Query.CompositeFilter(Query.CompositeFilterOperator.AND, dateFilters);
+        
+        List<Filter> filters = new ArrayList<Filter>();
+        filters.add(symbolFilter);
+        filters.add(dateFilter);
+        Query.CompositeFilter filter = new Query.CompositeFilter(Query.CompositeFilterOperator.AND, filters);
+        
+        query.setFilter(filter);
+        query.addSort("created", SortDirection.ASCENDING);
 
-	public List<String> retrieveStock(String[] symbols, Date fromDate, Date toDate) {
-		
-		PropertyFilter[] propertyArray = new PropertyFilter[symbols.length-1];
-		for (int i = 1; i < symbols.length; i++) {
-			propertyArray[i-1] = PropertyFilter.eq("symbol", symbols[i]);
-		}
-		CompositeFilter companiesFilter = CompositeFilter.and(PropertyFilter.eq("symbol", symbols[0]),propertyArray);
+        return formatStocks(datastoreService.prepare(query).asIterable());
+    }
 
-		
-		Query<Entity> query =
-			      Query.newEntityQueryBuilder()
-			      .setKind("stock")
-			      .setFilter(CompositeFilter.and(
-			    	        PropertyFilter.gt("created", Timestamp.of(fromDate)),
-			    	        PropertyFilter.lt("created", Timestamp.of(toDate))))
-			      .setFilter(companiesFilter)
-			      .setOrderBy(OrderBy.asc("created")).build();
-		
-		return formatStocks(datastore.run(query));
-	}
-	
-	  /**
-	   * Converts a list of stocks entities to a list of formatted stocks strings.
-	   *
-	   * @param stocks An iterator over task entities
-	   * @return A list of tasks strings, one per entity
-	   */
-	  static List<String> formatStocks(Iterator<Entity> stocks) {
-	    List<String> strings = new ArrayList<>();
-	    while (stocks.hasNext()) {
-	      Entity stock = stocks.next();
-	        strings.add(stock.toString());
-	    }
-	    return strings;
-	  }
+    /**
+     * Converts a list of stocks entities to a list of formatted stocks strings.
+     *
+     * @param stocks
+     *            An iterator over task entities
+     * @return A list of tasks strings, one per entity
+     */
+    static List<String> formatStocks(Iterable<Entity> stocks) {
+        List<String> strings = new ArrayList<>();
+        for (Entity stock : stocks) {
+            strings.add(stock.toString());
+        }
+        return strings;
+    }
 
 }
